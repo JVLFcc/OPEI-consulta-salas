ff4931e199dc17f8b712b2fe59581bb0708b1aa5
jvlf <jvlf@cin.ufpe.br>
2026-05-26 01:04:39 -0300

    chore: add project configuration files

---
+++ src/test/java/opei/cin/ufpe/br/consultasalas/ConsultaSalasApplicationTests.java
@@ -0,0 +1,13 @@
+package opei.cin.ufpe.br.consultasalas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaSalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
