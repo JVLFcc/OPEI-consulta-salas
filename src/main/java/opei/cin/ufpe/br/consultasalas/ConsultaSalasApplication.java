ca7123af65ebfb3c786cf0e1982ba2b046ed2745
jvlf <jvlf@cin.ufpe.br>
2026-05-26 00:49:12 -0300

    chore: configure initial Spring Boot project

---
+++ src/main/java/opei/cin/ufpe/br/consultasalas/ConsultaSalasApplication.java
@@ -0,0 +1,13 @@
+package opei.cin.ufpe.br.consultasalas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaSalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaSalasApplication.class, args);
+	}
+
+}
