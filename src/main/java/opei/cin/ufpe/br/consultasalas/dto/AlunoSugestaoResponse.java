b41fb2e037e8a2b3e77aa03c4c26d723fe4f636d
jvlf <jvlf@cin.ufpe.br>
2026-05-27 01:53:54 -0300

    feat: include birth date in student suggestions

--- src/main/java/opei/cin/ufpe/br/consultasalas/dto/AlunoSugestaoResponse.java
+++ src/main/java/opei/cin/ufpe/br/consultasalas/dto/AlunoSugestaoResponse.java
@@ -4,9 +4,12 @@ package opei.cin.ufpe.br.consultasalas.dto;
  * DTO usado na lista de sugestões do autocomplete
  *
  * retorna apenas dados necessários para o aluno se identificar sem informações sensíveis
+ *
+ * ? adicionei a data de nascimento, pq achei uma informação importante
  */
 public record AlunoSugestaoResponse(Long id,
                                     String nomeCompleto,
-                                    String cpfMascarado) {
+                                    String cpfMascarado,
+                                    String dataNascimento) {
 
 }
