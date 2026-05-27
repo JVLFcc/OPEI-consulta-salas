fe034acb584291efe8a6e6e1bb53d5e8c08fffa7
jvlf <jvlf@cin.ufpe.br>
2026-05-27 01:58:09 -0300

    feat: add repository query for student allocations by CPF

--- src/main/java/opei/cin/ufpe/br/consultasalas/repository/AlunoRepository.java
+++ src/main/java/opei/cin/ufpe/br/consultasalas/repository/AlunoRepository.java
@@ -6,21 +6,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 /**
- * repositório responsável por acessar dados de alunos no banco
- * o spring data JPA cria automaticamente as consultas com base nos métodos declarados na interface
+ * repositório responsável por consultar os alunos/alocacoes no banco
+ *
+ * cada linha da planilha representa uma alocação, então um aluno pode aparecer mais de uma vez
  */
 public interface AlunoRepository extends JpaRepository<Aluno, Long>{
 
     /**
-     * busca até 10 alunos os quais o nome contenha o termo informado, e ignora diferença entre maiúsculas e minúsculas
-     * ex.: "arthur" pode encontrar o "Arthur Santos de Lima"
+     * busca alunos pelo nome e é isso
+     * usa top20 agora pra n quebrar, e pq depois o service vai remover duplicatas por CPF
      */
-    List<Aluno> findTop10ByNomeCompletoContainingIgnoreCase(String nome);
+    List<Aluno> findTop20ByNomeCompletoContainingIgnoreCase(String nome);
 
     /**
-     * busca até 10 alunos os quais o cpf contenha o trecho informado
-     * cpf normalizado contém apenas números
+     * busca alunos pelo cpf normalizado e é isso
+     * cpf normalizado contém apenas números!
      */
-    List<Aluno> findTop10ByCpfNormalizadoContaining(String cpf);
+    List<Aluno> findTop20ByCpfNormalizadoContaining(String cpf);
+
+    /**
+     * busca todas alocações de um msm aluno pelo cpf
+     *
+     * esse método é o mais importante pra considerar os casos de que o aluno está em mais de uma modalidade
+     */
+    List<Aluno> findByCpfNormalizado(String cpfNormalizado);
 
 }
