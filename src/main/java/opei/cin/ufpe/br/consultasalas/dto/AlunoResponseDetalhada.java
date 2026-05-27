743d6746603bf975aca5781c989d97cbfcc909d0
jvlf <jvlf@cin.ufpe.br>
2026-05-27 01:57:36 -0300

    feat: return grouped student allocations in detail response

--- src/main/java/opei/cin/ufpe/br/consultasalas/dto/AlunoResponseDetalhada.java
+++ src/main/java/opei/cin/ufpe/br/consultasalas/dto/AlunoResponseDetalhada.java
@@ -1,17 +1,19 @@
 package opei.cin.ufpe.br.consultasalas.dto;
 
+import java.util.List;
+
 /**
  * DTO pra montar o card final que vai ser exibido para o aluno
  *
  * contém informações principais da consulta com destaque para o local de prova (sala, bloco e andar)
+ *
+ * ? agora ele rretorna uma lista de alocações, pois o mesmo aluno pode estar em mais de uma modalidade
  */
 public record AlunoResponseDetalhada(Long id,
                                      String nomeCompleto,
                                      String cpfMascarado,
-                                     String modalidade,
-                                     String polo,
-                                     String handle,
-                                     String sala,
-                                     String bloco,
-                                     String andar) {
+                                     String dataNascimento,
+                                     String instituicao,
+                                     List<AlocacaoResponse> alocacoes
+                                     ) {
 }
