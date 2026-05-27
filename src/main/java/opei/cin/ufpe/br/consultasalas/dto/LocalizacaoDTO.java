f4a7eed96a64d0ee8f32e51a568bd8cf5cca7b17
jvlf <jvlf@cin.ufpe.br>
2026-05-27 01:57:57 -0300

    feat: include formatted room in location DTO

--- src/main/java/opei/cin/ufpe/br/consultasalas/dto/LocalizacaoDTO.java
+++ src/main/java/opei/cin/ufpe/br/consultasalas/dto/LocalizacaoDTO.java
@@ -2,6 +2,12 @@ package opei.cin.ufpe.br.consultasalas.dto;
 
 /**
  * DTO bem simples apenas pra representar a localização derivada da sala
+ *
+ * salaFormatada é a sala já tratada pra exibição (por causa do G5 que tenho que transformar em GRAD 5)
+ *
+ * VC ME PAGA VICTOR SEU LIXO
  */
-public record LocalizacaoDTO(String bloco, String andar) {
+public record LocalizacaoDTO(String salaFormatada,
+                             String bloco,
+                             String andar) {
 }
